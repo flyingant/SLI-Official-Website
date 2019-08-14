8ada4b1f5aeba66f00c6888ff9d17b9c2b3cb809
mayi <mayi@wiredcraft.com>
2019-08-14 17:04:41 +0800

    A new try to website design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,1 @@
-console.log('Hello world!');
-$(document).ready(function () {
-  $(".case-study-carousel").slick({
-    autoplay: true,
-    autoplaySpeed: 2000,
-    arrows: false
-  });
-})
+AOS.init();
